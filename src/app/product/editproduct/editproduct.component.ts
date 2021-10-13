6978312319206689a695a77d38c6d173d200a263
kesh1502 <jootunkeshav@gmail.com>
2021-10-13 15:51:13 +0400

    edit products works

--- src/app/product/editproduct/editproduct.component.ts
+++ src/app/product/editproduct/editproduct.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
 import { ProductService } from '../../product.service';
 import { IProduct } from "c:/xampp/htdocs/Angular/frontend/src/app/product/product.module";
 import { FormBuilder, FormControl, Validators } from '@angular/forms';
@@ -6,50 +6,69 @@ import { HttpClient } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
 import { UserService } from 'src/app/user.service';
 
+
+
 @Component({
   selector: 'app-editproduct',
   templateUrl: './editproduct.component.html',
   styleUrls: ['./editproduct.component.css']
 })
 export class EditproductComponent implements OnInit {
+ 
   public products = [] as any;
-    public selectedProduct = <IProduct>{};
-    public modalTitle = '';
-    public btnTitle = '';
-    public name = new FormControl('',Validators.required);
-    public description = new FormControl('',Validators.required);
-    public price = new FormControl('',Validators.required);
-    public showError = false;
-    loggedIn: boolean=true;
+  public selectedProduct = <IProduct>{};
+  public modalTitle = '';
+  public btnTitle = '';
+  public name = new FormControl('',Validators.required);
+  public description = new FormControl('',Validators.required);
+  public price = new FormControl('',Validators.required);
+  public showError = false;
+ 
+  loggedIn: boolean=true;
+  modalService: any;
   
   constructor(private service: ProductService,private userService: UserService,private fb: FormBuilder,private http: HttpClient) { }
 
+  openModal(product?:IProduct) {
+    if(product){
+      this.modalTitle = 'Edit Product';
+      this.btnTitle = 'Update';
+      this.selectedProduct = product;
+      this.name.setValue(product.name);
+      this.description.setValue(product.description);
+      this.price.setValue(product.price);
+  } 
+}
+
   ngOnInit(): void {
+    
     this.userService.isUserLoggedIn().subscribe(
       status => this.loggedIn = status
     );
     console.log('isLogged', this.loggedIn);
-      this.getList();
+    this.getList();
+      
   }
 
   getList(){
     this.service.list()
       .subscribe(response => this.products = response);
   }
-    
+
   delete(product:IProduct){
-    this.service.delete(product).subscribe(response => this.getList());
+    this.service.delete(product)
+      .subscribe(response => this.getList());
   }
 
   save(){
     if(!this.name.value || !this.description.value || !this.price.value){
       this.showError = true;
       return;
-  }
+    }
 
-  this.selectedProduct.name = this.name.value;
-  this.selectedProduct.description = this.description.value;
-  this.selectedProduct.price = this.price.value;
+    this.selectedProduct.name = this.name.value;
+    this.selectedProduct.description = this.description.value;
+    this.selectedProduct.price = this.price.value;
 
     if(this.btnTitle == 'Update'){
       this.service.update(this.selectedProduct)
@@ -57,24 +76,9 @@ export class EditproductComponent implements OnInit {
           this.getList();
           this.reset();
           this.showError = false;
-        });
-    }else{
-      this.service.add(this.selectedProduct)
-        .subscribe(response=>{
-          this.getList();
-          this.reset();
-          this.showError = false;
+      
         });
     }
-  }
-
-  update(){
-    this.service.update(this.selectedProduct)
-      .subscribe(response=>{
-        this.getList();
-        this.reset();
-        this.showError = false;
-      });
   }
 
   reset(){
